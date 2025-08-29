857fa8f97f853bb9e69cabdc354caf4ff5a5617e
DaveBoyd <daveboyd777@gmail.com>
2025-08-29 18:29:49 -0400

    [ADD] Create FUNDING.yml for project support information and add module manifest for AIMaster Security System

--- aimaster-security-system.psm1
+++ aimaster-security-system.psm1
@@ -3,7 +3,7 @@ New-ModuleManifest -Path .\aimaster-security-system.psd1 `
   -ModuleVersion '1.0.0' `
   -Author 'daveboyd777' `
   -Description 'Security system module for AIMaster project' `
-  -CompanyName ’SoftfloTechnology `
-  -LicenseUri 'https://opensource.org/licenses/MIT `
+  -CompanyName 'SoftfloTechnology' `
+  -LicenseUri 'https://opensource.org/licenses/MIT' `
   -ProjectUri 'https://github.com/daveboyd777/aimaster-security-system' `
   -Tags 'security','aimaster','powershell','automation'
